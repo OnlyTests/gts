0ec9b57f2e3d5a00d650e479e8e6d97b110f0539
Jsousa <jsousa@BR0266.local>
2020-11-26 11:06:18 -0300

    update test

--- src/screens/Register/tests/index.test.js
+++ src/screens/Register/tests/index.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {render} from '@testing-library/react-native';
 import RegisterScreen from '../index';
 describe('RegisterScreen, check if render success', () => {
+  afterEach(jest.clearAllMocks);
+  jest.useFakeTimers();
+
   test('render', async () => {
     const {toJSON, unmount} = await render(<RegisterScreen />);
     expect(toJSON()).toMatchSnapshot();
